3a91072f34f105a193651ed1a814869be2cff1f9
JorgeEscrichCIEEP <jorge.escrichlaorden@cieep.com>
2024-05-21 10:55:14 +0200

    Arreglo de ruta a componente JorgeEscrichLaorden

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,15 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { VicenteAlacreuComponent } from './vicente-alacreu/vicente-alacreu.component';
 import { MarcosGomezComponent } from './marcos-gomez/marcos-gomez.component';
+import { JorgeEscrichLaordenComponent } from './components/jorge-escrich-laorden/jorge-escrich-laorden.component';
 
-
-=======
 
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
     { path: 'marcos-gomez', component: MarcosGomezComponent },
     {path: 'home', component: HomeComponent},
-    {path: 'component/jorge-escrich-laorden', component: jorge-escrich-laorden},
+    {path: 'jorge-escrich-laorden', component: JorgeEscrichLaordenComponent},
     {path: 'navbar', component: NavbarComponent},
     { path: '**', component: PageNotFoundComponent }, 
     
