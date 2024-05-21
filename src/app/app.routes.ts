a4f8cf19c89c18ee01fdbcf6c7d9bbdca64300ae
VicenteAlacreu <vicente.alacreusilvestre@cieep.com>
2024-05-21 10:20:17 +0200

    Crear componente VicenteAlacreu y ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { VicenteAlacreuComponent } from './vicente-alacreu/vicente-alacreu.component';
 
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
     {path: 'navbar', component: NavbarComponent},
+    { path: 'valacreu', component: VicenteAlacreuComponent},
     { path: '**', component: PageNotFoundComponent }, 
+    
 ];
