87b98c27013d5951be9a75f2655600b0ea7da1de
AlvaroGS <alvaro.garciasanchez@cieep.com>
2024-05-21 10:54:10 +0200

    He metido mi componente en la carpeta componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AlvaroGarciaComponent } from './alvaro-garcia/alvaro-garcia.component';
+import { AlvaroGarciaComponent } from './components/alvaro-garcia/alvaro-garcia.component';
 
 
 export const routes: Routes = [
