f2d39d1fa9e6fd3959a30b6ce7e30c62075a2a0b
AlvaroSJ <alvaro.sanjoseyagues@cieep.com>
2024-05-21 11:24:42 +0200

    se ha arreglado el link del nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AlvarosanjoseComponent } from './components/alvarosanjose/alvarosanjose.component';
 
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
     {path: 'navbar', component: NavbarComponent},
+    {path: "alvarosanjose", component:AlvarosanjoseComponent},
     { path: '**', component: PageNotFoundComponent }, 
 ];
