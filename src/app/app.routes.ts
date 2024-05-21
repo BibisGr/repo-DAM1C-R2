c6bedc4384be667b6215951c2d1e922995529676
MarcosG <MarcosG@gmail.com>
2024-05-21 10:28:45 +0200

    Añadir componente MarcosGomez y crear ruta para el componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+import { MarcosGomezComponent } from './marcos-gomez/marcos-gomez.component';
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
+    { path: 'marcos-gomez', component: MarcosGomezComponent },
     {path: 'home', component: HomeComponent},
     {path: 'navbar', component: NavbarComponent},
     { path: '**', component: PageNotFoundComponent }, 
