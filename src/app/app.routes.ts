1f84f4363b2c4ccf6f7f830d6978959894b2c667
Adrian <Adrianamat@gmail.com>
2024-05-21 10:35:39 +0200

    crear componente y crear ruta para el componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { InfoComponent } from './components/info/info.component';
 
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
     {path: 'navbar', component: NavbarComponent},
-    { path: '**', component: PageNotFoundComponent }, 
+    { path: '**', component: PageNotFoundComponent },
+    { path: 'info', component: InfoComponent } 
 ];
