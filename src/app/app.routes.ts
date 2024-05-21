8f7b87fcf73b3af6f312f301d6dd503d8cd14e52
BerthaGr <BerthaGr@gmail.com>
2024-05-21 11:50:33 +0200

    Fix Navbar 3 Bertha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,26 +2,21 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-<<<<<<< HEAD
 import { MarcosGomezComponent } from './components/marcos-gomez/marcos-gomez.component';
-=======
 import { AlvaroGarciaComponent } from './components/alvaro-garcia/alvaro-garcia.component';
 import { VicenteAlacreuComponent } from './components/vicente-alacreu/vicente-alacreu.component';
-import { MarcosGomezComponent } from './marcos-gomez/marcos-gomez.component';
 import { JorgeEscrichLaordenComponent } from './components/jorge-escrich-laorden/jorge-escrich-laorden.component';
 import { AlfonsoComponent } from './components/alfonso/alfonso.component';
-
 import { AlvarosanjoseComponent } from './components/alvarosanjose/alvarosanjose.component';
 
->>>>>>> main
 
 export const routes: Routes = [
-    { path: 'marcos-gomez', component: MarcosGomezComponent },
-    {path: 'AlvaroGarcia',component:AlvaroGarciaComponent},
+    {path: 'home', component: HomeComponent},
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
     { path: 'marcos-gomez', component: MarcosGomezComponent },
+    {path: 'AlvaroGarcia',component:AlvaroGarciaComponent},
+    { path: 'marcos-gomez', component: MarcosGomezComponent },
     { path: 'VicenteAlc', component: VicenteAlacreuComponent }, // is OK
-    {path: 'home', component: HomeComponent},
     {path: 'jorge-escrich-laorden', component: JorgeEscrichLaordenComponent},
     {path: 'navbar', component: NavbarComponent},
     { path: 'Alfonso', component: AlfonsoComponent},
