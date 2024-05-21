be45fedb7066ff08696be16ad18f42ad3ecbae63
AlvaroGS <alvaro.garciasanchez@cieep.com>
2024-05-21 10:33:46 +0200

    Agregado en el componente navbar mi nombre y linkeado a mi componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AlvaroGarciaComponent } from './alvaro-garcia/alvaro-garcia.component';
 
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
+    {path: 'AlvaroGarcia',component:AlvaroGarciaComponent},
     {path: 'home', component: HomeComponent},
     {path: 'navbar', component: NavbarComponent},
     { path: '**', component: PageNotFoundComponent }, 
